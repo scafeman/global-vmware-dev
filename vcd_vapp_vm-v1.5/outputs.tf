508b9934efcaf16b4fa202decae6989ee2c7a9d5
Matt Scafe <matt.scafe@rackspace.com>
2023-04-28 11:33:45 -0500

    Updated the IP output line to use the proper value (vm.network[*].ip)

--- vcd_vapp_vm-v1.5/outputs.tf
+++ vcd_vapp_vm-v1.5/outputs.tf
@@ -1,22 +1,22 @@
 output "vm_info" {
   value = [
-    for vm in vcd_vapp_vm.this : {
-      name           = vm.name
-      ip_address     = vm.ip
-      computer_name  = vm.computer_name
-      metadata_entries = vm.metadata_entry
+    for vm in vcd_vapp_vm.vm : {
+      name              = vm.name
+      ip                = vm.network[*].ip
+      computer_name     = vm.computer_name
+      metadata_entries  = vm.metadata_entry
     }
   ]
 }
 
 output "all_vm_info" {
   value = {
-    for vm in vcd_vapp_vm.this :
+    for vm in vcd_vapp_vm.vm :
     vm.name => {
-      ip_address     = vm.ip
-      computer_name  = vm.computer_name
-      metadata       = vm.metadata_entry
-      sizing_policy  = data.vcd_vm_sizing_policy.sizing_policy.name
+      ip              = vm.network[*].ip
+      computer_name   = vm.computer_name
+      metadata        = vm.metadata_entry
+      sizing_policy   = data.vcd_vm_sizing_policy.sizing_policy.name
     }
   }
 }
