f8ba15db7efa84841dc3bd91cdbe6610e7c7f288
Matt Scafe <matt.scafe@rackspace.com>
2023-05-09 16:02:26 -0500

    Added conditional expressions to include the required arguments based on the rule type

--- vcd_nsxt_nat_rule/main.tf
+++ vcd_nsxt_nat_rule/main.tf
@@ -11,31 +11,31 @@ terraform {
 
 # Create the Datacenter Group data source
 data "vcd_vdc_group" "dcgroup" {
-  name    = var.vdc_group_name
+  name      = var.vdc_group_name
 }
 
 # Create the NSX-T Edge Gateway data source
 data "vcd_nsxt_edgegateway" "t1" {
-  org      = var.vdc_org_name
-  owner_id = data.vcd_vdc_group.dcgroup.id
-  name     = var.vdc_edge_name
+  org       = var.vdc_org_name
+  owner_id  = data.vcd_vdc_group.dcgroup.id
+  name      = var.vdc_edge_name
 }
-
 
 # Create the vcd_nsxt_nat_rule resource block using for_each to loop through the nat_rules list
 resource "vcd_nsxt_nat_rule" "nat_rules" {
-  for_each         = var.nat_rules
-  org              = var.vdc_org_name
-  edge_gateway_id  = data.vcd_nsxt_edgegateway.t1.id
+  for_each                  = var.nat_rules
+  org                       = var.vdc_org_name
+  edge_gateway_id           = data.vcd_nsxt_edgegateway.t1.id
   name                      = each.value.name
   rule_type                 = each.value.rule_type
-  description               = each.value.description
-  external_address          = each.value.external_address
-  internal_address          = each.value.internal_address
-  snat_destination_address  = each.value.snat_destination_address
-  dnat_external_port        = each.value.dnat_external_port
-  app_port_profile_id       = each.value.app_port_profile_id
-  logging                   = each.value.logging
-  firewall_match            = each.value.firewall_match
-  priority                  = each.value.priority
+
+  description               = each.value.description != null ? each.value.description : null
+  external_address          = each.value.external_address != null ? each.value.external_address : null
+  internal_address          = each.value.internal_address != null ? each.value.internal_address : null
+  snat_destination_address  = each.value.rule_type == "SNAT" && each.value.snat_destination_address != null ? each.value.snat_destination_address : null
+  dnat_external_port        = each.value.rule_type == "DNAT" && each.value.dnat_external_port != null ? each.value.dnat_external_port : null
+  app_port_profile_id       = each.value.app_port_profile_id != null ? each.value.app_port_profile_id : null
+  logging                   = each.value.logging != null ? each.value.logging : null
+  firewall_match            = each.value.firewall_match != null ? each.value.firewall_match : null
+  priority                  = each.value.priority != null ? each.value.priority : null
 }
--- vcd_nsxt_nat_rule/variables.tf
+++ vcd_nsxt_nat_rule/variables.tf
@@ -6,33 +6,53 @@ variable "vdc_group_name" {}
 
 variable "nat_rules" {
   type = map(object({
-    rule_type             = string
-    name                  = string
-    description           = optional(string)
-    external_address      = optional(string)
-    internal_address      = optional(string)
-    snat_destination_address = optional(string)
-    dnat_external_port    = optional(string)
-    app_port_profile_id   = optional(string)
-    logging               = optional(bool)
-    firewall_match        = optional(string)
-    priority              = optional(number)
+    rule_type                   = string
+    name                        = string
+    description                 = optional(string)
+    external_address            = optional(string)
+    internal_address            = optional(string)
+    snat_destination_address    = optional(string)
+    dnat_external_port          = optional(string)
+    app_port_profile_id         = optional(string)
+    logging                     = optional(bool)
+    firewall_match              = optional(string)
+    priority                    = optional(number)
   }))
   default = {
     snat_rule = {
-      rule_type             = "SNAT"
-      name                  = "192.168.0.0/24_SNAT"
-      external_address      = "8.8.8.8"
-      internal_address      = "192.168.0.0/24"
-      logging               = false
+      rule_type                 = "SNAT"
+      name                      = "192.168.0.0/24_SNAT"
+      external_address          = "8.8.8.8"
+      internal_address          = "192.168.0.0/24"
+      logging                   = false
     },
     dnat_rule = {
-      rule_type             = "DNAT"
-      name                  = "192.168.0.10_DNAT-HTTP"
-      external_address      = "8.8.8.8"
-      internal_address      = "192.168.0.10"
-      dnat_external_port    = "80"
-      logging               = false
+      rule_type                 = "DNAT"
+      name                      = "192.168.0.10_DNAT-HTTP"
+      external_address          = "8.8.8.8"
+      internal_address          = "192.168.0.10"
+      dnat_external_port        = "80"
+      logging                   = false
+    },
+    no_dnat_rule = {
+      rule_type                 = "NO_DNAT"
+      name                      = "192.168.0.0/24_NO_DNAT"
+      external_address          = "8.8.8.8"
+      logging                   = false
+    },
+    no_snat_rule = {
+      rule_type                 = "NO_SNAT"
+      name                      = "192.168.0.0/24_NO_SNAT"
+      internal_address          = "192.168.0.0/24"
+      logging                   = false
+    },
+    reflexive_rule = {
+      rule_type                 = "REFLEXIVE"
+      name                      = "192.168.0.10_REFLEXIVE"
+      external_address          = "8.8.8.8"
+      internal_address          = "192.168.0.10"
+      snat_destination_address  = "192.168.0.1"
+      logging                   = false
     }
   }
 }
